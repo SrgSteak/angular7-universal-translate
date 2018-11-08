54064f392721262af995a883db67f0d24febb2bc
Robin Schmidt <schmidt91robin@icloud.com>
2018-11-08 20:30:53 +0100

    removed debug output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent  {
 
-  constructor(private localizeRouterService: LocalizeRouterService, router: Router) {
-    console.log('=================================');
-    console.log(router.config[0].children[0]); // all routes are double??
-    console.log(router.config[0].children[1]);
-    // console.log(router.config[0].children[2]);
-    console.log('=================================');
-  }
+  constructor(private localizeRouterService: LocalizeRouterService, router: Router) {}
 
   switchLang(lang: string): void {
     this.localizeRouterService.changeLanguage(lang);
