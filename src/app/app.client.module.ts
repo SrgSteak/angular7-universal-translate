9a1d7b722c77fd43e2bc36c3a6b8d17baacc964e
Robin Schmidt <schmidt91robin@icloud.com>
2018-11-08 20:38:27 +0100

    removed more clutter

--- src/app/app.client.module.ts
+++ src/app/app.client.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from "@angular/core";
-import { TranslateModule, TranslateLoader, TranslateService, TranslatePipe } from "@ngx-translate/core";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TranslateService } from "@ngx-translate/core";
+import { HttpClientModule } from "@angular/common/http";
 import { LocalizeRouterModule, LocalizeParser, LocalizeRouterSettings, ManualParserLoader } from "@gilsdav/ngx-translate-router";
-import { LocalizeRouterHttpLoader } from "@gilsdav/ngx-translate-router-http-loader";
 import { Routes, RouterModule } from "@angular/router";
 import { ListComponent } from "./pages/shop/list/list.component";
 import { Location } from '@angular/common';
 import { BetaComponent } from "./beta.component";
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 export function createTranslateLoader(translate: TranslateService, location: Location, settings: LocalizeRouterSettings) {
   return new ManualParserLoader(translate, location, settings, ['en', 'de'], 'ROUTES.');
@@ -36,13 +29,6 @@ export const routes: Routes = [
 @NgModule({
   imports: [
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-    }),
     LocalizeRouterModule.forRoot(routes, {
       parser: {
         provide: LocalizeParser,
